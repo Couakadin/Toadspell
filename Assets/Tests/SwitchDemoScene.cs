46bc40feab45abf690b38285fa4137d39556f411
Pauline <pauline613@msn.com>
2024-10-11 11:10:03 +0200

    feat: spell scene
    Test Scene is sending to spell scene

--- Assets/Tests/SwitchDemoScene.cs
+++ Assets/Tests/SwitchDemoScene.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,7 +7,7 @@ public class SwitchDemoScene : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
             Application.Quit();
         }
