cf6c22f3822b39f01aa5c6d029623b162b1b2062
Pauline <pauline613@msn.com>
2024-10-15 09:48:51 +0200

    feat: enemy set up

---
+++ Assets/_/Features/Characters/Enemies/Behaviours/EnemyBase.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies.Runtime
+{
+    public class EnemyBase : MonoBehaviour
+    {
+        #region Publics
+	
+        #endregion
+
+
+        #region Unity API
+		
+    	void Start()
+    	{
+	
+    	}
+
+    	void Update()
+    	{
+	
+    	}
+	
+        #endregion
+	
+	
+        #region Main Methods
+	
+        #endregion
+	
+	
+        #region Utils
+	
+        #endregion
+	
+	
+        #region Privates & Protected
+	
+        #endregion
+    }
+}
