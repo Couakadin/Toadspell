407ce130369ac870edd9013d2e2820d3b37997ab
Pauline <pauline613@msn.com>
2024-10-23 17:05:24 +0200

    feat: dialogues template

---
+++ Assets/_/Features/Data/Scriptables/Dialogue.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data.Runtime
+{
+    [CreateAssetMenu(fileName = nameof(Dialogue), menuName = Consts.SCRIPTABLES_LINES + nameof(Dialogue))]
+    public class Dialogue : ScriptableObject
+    {
+        #region Publics
+
+        public Sprite m_image;
+        public List<DialogueLine> m_lines;
+
+        #endregion
+    }
+}
