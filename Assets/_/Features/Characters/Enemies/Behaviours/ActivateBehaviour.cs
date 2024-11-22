d2a8a18bd2a0f59b5f2a827e489932ecea62b57b
Valentin Saint-Jean <webdev.valentin@gmail.com>
2024-11-22 10:01:27 +0100

    feat: activate script

---
+++ Assets/_/Features/Characters/Enemies/Behaviours/ActivateBehaviour.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Enemies.Runtime
+{
+    public class ActivateBehaviour : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (((1 << other.gameObject.layer) & _layerMask) == 0 || _script.enabled) return;
+            _script.enabled = true;
+        }
+
+        [Header("Activate Settings")]
+        [Tooltip("Target layer that triggers activation.")]
+        [SerializeField]
+        private LayerMask _layerMask;
+        [Tooltip("Target to activate.")]
+        [SerializeField]
+        private MonoBehaviour _script;
+    }
+}
