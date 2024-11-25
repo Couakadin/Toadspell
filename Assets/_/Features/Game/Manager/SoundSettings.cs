64434550132290af3a33573ab0370e94e502c1a2
Pauline <pauline613@msn.com>
2024-11-25 11:07:04 +0100

    feat: sound manager

---
+++ Assets/_/Features/Game/Manager/SoundSettings.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace Game.Runtime
+{
+    public class SoundSettings : MonoBehaviour
+    {
+        #region Publics
+	
+        #endregion
+
+
+        #region Unity API
+		
+    	void Start()
+    	{
+	       
+    	}
+
+    	void Update()
+    	{
+	
+    	}
+
+        #endregion
+
+
+        #region Main Methods
+
+        public void SetEnvironmentVolume()
+        {
+            SetVolume(_environmentSlider.value, _environmentSlider, "EnvironmentVolume");
+        }
+
+        public void SetVFXVolume()
+        {
+            SetVolume(_vfxSlider.value, _vfxSlider, "SfxVolume");
+        }
+
+        #endregion
+
+
+        #region Utils
+
+        private void SetVolume(float value, Slider slider, string nameOfAudio)
+        {
+            if (value < 1)
+            {
+                value = .001f;
+            }
+            RefreshSlider(value, slider);
+            _audioMixer.SetFloat(nameOfAudio, Mathf.Log10(value / 100) * 20f);
+
+        }
+
+        //private void SetVolumeFromslider(Slider slider)
+        //{
+        //    SetVolume(slider.value, slider);
+        //}
+
+        private void RefreshSlider(float value, Slider slider)
+        {
+            slider.value = value;
+        }
+
+        #endregion
+
+
+        #region Privates & Protected
+
+        [SerializeField] private Slider _environmentSlider;
+        [SerializeField] private Slider _vfxSlider;
+        [SerializeField] private AudioMixer _audioMixer;
+        #endregion
+    }
+}
