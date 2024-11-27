1382436c3167f23ef3d78353c1a21b48bfd19867
Pauline <pauline613@msn.com>
2024-11-27 13:01:51 +0100

    feat: enemies lifebar

--- Assets/_/Features/Characters/Objects/Behaviours/HealthBarBillboardBehaviour.cs
+++ Assets/_/Features/Characters/Objects/Behaviours/HealthBarBillboardBehaviour.cs
@@ -9,7 +9,7 @@ namespace Objects.Runtime
         private void LateUpdate()
         {
             transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-            transform.Rotate(0, 180, 0);
+            //transform.Rotate(0, 180, 0);
         }
     }
 }
